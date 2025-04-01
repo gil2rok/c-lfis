247714c80ed88d26119f8c59b1443e9f4519299c
Gilead Turok <gturok@workergpu151.cm.cluster>
2025-04-01 17:56:28 -0400

    Refactor :w

--- clifs.py
+++ clifs.py
@@ -38,6 +38,20 @@ def target_logdensity_fn(x: Array) -> Scalar:
     return jsp.special.logsumexp(log_mixtures + log_weights)
 
 
+def target_sample_fn(rng_key: Key, num_samples: int = 1) -> Array:
+    m1_key, m2_key, m3_key, m4_key, cat_key = jr.split(rng_key, 5)
+    m1 = jr.multivariate_normal(m1_key, jnp.array([2.0, 2.0]), jnp.eye(2), (num_samples,))
+    m2 = jr.multivariate_normal(m2_key, jnp.array([-2.0, -2.0]), jnp.eye(2), (num_samples,))
+    m3 = jr.multivariate_normal(m3_key, jnp.array([2.0, -2.0]), jnp.eye(2), (num_samples,))
+    m4 = jr.multivariate_normal(m4_key, jnp.array([-2.0, 2.0]), jnp.eye(2), (num_samples,))
+    mixtures = jnp.stack([m1, m2, m3, m4], axis=0)  # shape (4, num_samples, dim)
+    log_weights = jnp.array([0.25, 0.25, 0.25, 0.25])
+    categories = jr.categorical(cat_key, logits=log_weights, shape=(num_samples,))
+    one_hot_categories = jax.nn.one_hot(categories, num_classes=4) # shape (num_samples, 4)
+    # TODO: multiply mixtures by one-hot categories to get final samples
+    return None
+
+
 def probability_path_logdensity_fn(x: Array, time: Scalar) -> Scalar:
     """Logdensity of time-dependent probability path that linearly interpolates 
     btwn a base distribution and an unnormalized target distribution."""
@@ -47,7 +61,9 @@ def probability_path_logdensity_fn(x: Array, time: Scalar) -> Scalar:
 def divergence(fn: Callable[[Array], Array]) -> Callable[[Array], Scalar]:
     """Compute the divergence of a vector field for calleable fn: R^d -> R^d.
 
-    Naive implementation computes entire Jacobian even though only diagonal is needed.
+    This implementation inefficiently computes the entire Jacobian in O(n*(n+1)/2) even
+    though only diagonal in O(n) is needed.
+    
     Code from: https://github.com/jax-ml/jax/issues/3022#issuecomment-2100553108.
     """
     return lambda x: jnp.trace(jax.jacobian(fn)(x))
@@ -67,8 +83,8 @@ class Velocity(eqx.Module):
         self, 
         in_size: int, 
         out_size: int, 
-        width_size: int = 256, # previously 64
-        depth: int = 8, # previously 2
+        width_size: int = 256, # previously 256
+        depth: int = 8, # previously 8
         *,
         key: Key
     ):
@@ -94,6 +110,39 @@ class LFISState(NamedTuple):
 
 class LFISInfo(NamedTuple):
     loss: Array
+    x_t: Array
+    
+
+class LFISMetrics(NamedTuple):
+    wasserstein_distance: Array
+    true_continuity_error: Array
+    approx_continuity_error: Array
+
+
+def compute_1D_wasserstein_distance(X, Y):
+    """Efficiently compute 1D Wasserstein distance between two point clouds."""
+    return jnp.mean(jnp.abs(jnp.sort(X, axis=0) - jnp.sort(Y, axis=0)))
+    
+    
+def metrics(rng_key, LFISState, LFISInfo) -> LFISMetrics:
+    target_key, approx_key = jr.split(rng_key, 2)
+    num_samples = LFISInfo.x_t.shape[0]
+    target_samples = target_sample_fn(target_key, num_samples)
+    approx_samples = sample(
+        approx_key, 1, LFISState.params, LFISState.static, base_sample_fn, num_samples
+    )
+    wasserstein_distance = compute_1D_wasserstein_distance(target_samples, approx_samples)
+    true_continuity_error = continuity_error(
+        LFISState.params, LFISState.static, target_samples, 1.0
+    )
+    approx_continuity_error = continuity_error(
+        LFISState.params, LFISState.static, approx_samples, 1.0
+    )
+    return LFISMetrics(
+        wasserstein_distance=wasserstein_distance,
+        true_continuity_error=true_continuity_error,
+        approx_continuity_error=approx_continuity_error,
+    )   
 
 
 def init(params: PyTree, optimizer: GradientTransformation) -> LFISState:
@@ -101,60 +150,98 @@ def init(params: PyTree, optimizer: GradientTransformation) -> LFISState:
     return LFISState(params=params, opt_state=opt_state)
 
 
+def continuity_error(
+    params: PyTree,
+    static: PyTree,
+    x_t: Array,
+    time: Scalar,
+) -> Array:
+    """Compute error in continuity equation at time t for multiple points x_t ~ p_t."""
+    velocity = eqx.combine(params, static)
+
+    def vmap_me_plz(x_t: Array, time: Scalar) -> tuple:
+        """Clean way to batch multiple computations over `num_samples`."""
+        vel = velocity(x_t, time) # shape (dim,)
+        div = divergence(lambda x: velocity(x, time))(x_t)  # shape ()
+        score = jax.grad(probability_path_logdensity_fn, argnums=0)(x_t, time)  # shape (dim,)
+        time_partial = jax.grad(probability_path_logdensity_fn, argnums=1)(x_t, time)  # shape ()
+        return vel, div, score, time_partial
+
+    vel, div, score, time_partial = jax.vmap(vmap_me_plz, in_axes=(0, None))(x_t, time)
+    partial_t_Z = jnp.mean(time_partial, axis=0) # shape (num_samples,)
+    return div + jnp.vecdot(vel, score) + time_partial - partial_t_Z # shape (num_samples,)
+
+
 @eqx.filter_jit
 def step(
     rng_key: Key,
     state: LFISState,
     time: Scalar,
-    probability_path_logdensity_fn: Callable,
     optimizer: GradientTransformation,
     static: PyTree,
     num_samples: int = 1,
 ) -> tuple[LFISState, LFISInfo]:
-    velocity = eqx.combine(state.params, static)
-    x_t = sample(rng_key, time, velocity, base_sample_fn, num_samples)  # shape (num_samples, dim)
-        
-    def continuity_error(params: PyTree, x_t: Array, time: Scalar) -> Scalar:
-        """Compute (squared) error in continuity equation at time t averaged over 
-        multiple locations x_t ~ p_t."""
-        velocity = eqx.combine(params, static)
+    x_t = sample(
+        rng_key, time, state.params, static, base_sample_fn, num_samples
+    )  # shape (num_samples, dim), shape (num_samples,)
+    
+    def continuity_error_loss_fn(params: PyTree) -> Scalar:
+        """Mean squared error in continuity equation at time t."""
+        eps = continuity_error(params, static, x_t, time)
+        return jnp.mean(eps ** 2, axis=0)
 
-        def vmap_me_plz(x_t: Array, time: Scalar) -> tuple:
-          """Clean way to batch/vmap multiple computations."""
-          vel = velocity(x_t, time) # shape (dim,)
-          div = divergence(lambda x: velocity(x, time))(x_t)  # shape ()
-          score = jax.grad(probability_path_logdensity_fn, argnums=0)(x_t, time)  # shape (dim,)
-          time_partial = jax.grad(probability_path_logdensity_fn, argnums=1)(x_t, time)  # shape ()
-          return vel, div, score, time_partial
-
-        vel, div, score, time_partial = jax.vmap(vmap_me_plz, in_axes=(0, None))(x_t, time)
-        partial_t_Z = jnp.mean(time_partial, axis=0) # shape ()
-        eps = div + jnp.vecdot(vel, score) + time_partial - partial_t_Z
-        return jnp.mean(eps**2, axis=0)
-
-    loss, grads = jax.value_and_grad(continuity_error)(state.params, x_t, time)
-    assert grads is not None
+    loss, grads = jax.value_and_grad(continuity_error_loss_fn)(state.params)
     updates, new_opt_state = optimizer.update(grads, state.opt_state)
     new_params = optax.apply_updates(state.params, updates)
-    return LFISState(params=new_params, opt_state=new_opt_state), LFISInfo(loss=loss)
+    return LFISState(params=new_params, opt_state=new_opt_state), LFISInfo(loss=loss, x_t=x_t)
 
 
 def sample(
     rng_key: Key,
     time: Scalar,
-    velocity: eqx.Module,
+    params: PyTree,
+    static: PyTree,
     base_sample_fn: Callable,
     num_samples: int = 1,
+    delta_t: Scalar = 0.005,
 ) -> Array:
     x_0 = base_sample_fn(rng_key, num_samples) # shape (num_samples, dim)
-    dt = 0.02
+    velocity = eqx.combine(params, static)
+    euler_step = lambda x_t, time: x_t + delta_t * velocity(x_t, time)
+    vmap_euler_step = jax.vmap(euler_step, in_axes=(0, None))
+    
+    def body_fn(time, carry):
+        x_t = carry
+        x_t = vmap_euler_step(x_t, time)
+        return x_t
+    
+    x_t = jax.lax.fori_loop(
+        lower=1,
+        upper=1 + jnp.array(time / delta_t, dtype=int),
+        body_fun=lambda time_idx, carry: body_fn(time_idx / num_time_steps, carry),
+        init_val=x_0,
+    )
+    return x_t
+
+
+def _sample(
+    rng_key: Key,
+    time: Scalar,
+    params: PyTree,
+    static: PyTree,
+    base_sample_fn: Callable,
+    num_samples: int = 1,
+    delta_t: Scalar = 0.02,
+) -> Array:
+    x_0 = base_sample_fn(rng_key, num_samples) # shape (num_samples, dim)
+    velocity = eqx.combine(params, static)
     
     def euler_step(x, time):
-        return x + dt * velocity(x, time)
+        return x + delta_t * velocity(x, time)
     
     euler_integrator = lambda x0, time: jax.lax.fori_loop(
         lower=1,
-        upper=1 + jnp.array(time / dt, dtype=int),
+        upper=1 + jnp.array(time / delta_t, dtype=int),
         body_fun=lambda i, x: euler_step(x, i / num_time_steps),
         init_val=x0
     )
@@ -182,18 +269,17 @@ def main(seed: int, num_time_steps: int, num_train_steps: int, num_samples: int)
                 step_key,
                 state,
                 time,
-                probability_path_logdensity_fn,
                 optimizer,
                 static,
                 num_samples,
             )
             if j % 25 == 0:
-                print(f"Step {j} Loss: {info.loss}\tGrad Norm: {info.grad_norm}")
+                print(f"Step {j} Loss: {info.loss}")
 
     # generate approximate samples and plot them
     rng_key, sub_key = jr.split(rng_key)
     velocity = eqx.combine(state.params, static)
-    approx_samples = sample(sub_key, 1, velocity, base_sample_fn, num_samples)
+    approx_samples = sample(sub_key, 1, state.params, static, base_sample_fn, num_samples)
     sns.scatterplot(x=approx_samples[:, 0], y=approx_samples[:, 1], alpha=0.5)
     plt.savefig('hist.png', dpi=300)
 
@@ -201,7 +287,7 @@ def main(seed: int, num_time_steps: int, num_train_steps: int, num_samples: int)
 if __name__ == "__main__":
     seed = 12345
     num_time_steps = 200
-    num_train_steps = 250
+    num_train_steps = 500
     num_samples = 1000
     
     main(seed, num_time_steps, num_train_steps, num_samples)
