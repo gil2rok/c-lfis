16ef492567ab3f8bc14cbce2c7cd10ef0f1969cb
Gilead Turok <gturok@workergpu169.cm.cluster>
2025-04-07 15:11:01 -0400

    Bug fixes and refactor
    Fix two bugs in `sample()` function in `lfis.py`:
        - adaptively pick final time step `delta_t` to hit target
        integration time `t`
        - numerical integration step from time `t` to `t + delta_t` now
        evaluates the neural veloctiy field at time `t` instead of time
        `t + delta_t`
    
    Refactor:
        - sample time from `t ~ beta(4,4)` instead of uniform
        - exponential moving average over nn parameters
        - single gradient update per individual time step (instead
        of ~200 previously)

--- lfis.py
+++ lfis.py
@@ -24,6 +24,7 @@ class LFISState(NamedTuple):
 
 class LFISInfo(NamedTuple):
     loss: Array
+    time: Scalar
     x_t: Array
 
 
@@ -36,23 +37,21 @@ def init(params: PyTree, optimizer: GradientTransformation) -> LFISState:
 def step(
     rng_key: Key,
     state: LFISState,
-    time: Scalar,
     optimizer: GradientTransformation,
     static: PyTree,
     base_sample_fn: Callable,
     probability_path_logdensity_fn: Callable,
     num_samples: int = 1,
-    num_time_steps: int = 200,
-    delta_t: Scalar = 0.005,
 ) -> tuple[LFISState, LFISInfo]:
+    time_key, sample_key = jr.split(rng_key)
+    time = jr.beta(time_key, 4, 4) # shape ()
     x_t = sample(
-        rng_key=rng_key,
+        rng_key=sample_key,
         time=time,
         params=state.params, 
         static=static, 
         base_sample_fn=base_sample_fn, 
         num_samples=num_samples, 
-        delta_t=delta_t
     )  # shape (num_samples, dim)
     
     def continuity_error_loss_fn(params: PyTree) -> Scalar:
@@ -64,8 +63,14 @@ def step(
 
     loss, grads = jax.value_and_grad(continuity_error_loss_fn)(state.params)
     updates, new_opt_state = optimizer.update(grads, state.opt_state)
-    new_params = optax.apply_updates(state.params, updates)
-    return LFISState(params=new_params, opt_state=new_opt_state), LFISInfo(loss=loss, x_t=x_t)
+    new_params = optax.incremental_update(
+        new_tensors = optax.apply_updates(state.params, updates),
+        old_tensors = state.params,
+        step_size = 0.999
+    ) # exponential moving average btwn prev params and new params from optimizer update
+    new_state = LFISState(params=new_params, opt_state=new_opt_state)
+    new_info = LFISInfo(loss=loss, time=time, x_t=x_t)
+    return new_state, new_info
 
 
 def sample(
@@ -77,22 +82,24 @@ def sample(
     num_samples: int = 1,
     delta_t: Scalar = 0.005,
 ) -> Array:
+    """Evolve samples from x_0 ~ p_0 to x_t ~ p_t using neural velocity field."""
     x_0 = base_sample_fn(rng_key, num_samples) # shape (num_samples, dim)
     velocity = eqx.combine(params, static)
     euler_step = lambda x_t, time, delta_t: x_t + delta_t * velocity(x_t, time)
     vmap_euler_step = jax.vmap(euler_step, in_axes=(0, None, None))
     
     def body_fn(time, carry, delta_t):
+        """Apply numerical integration step from time t to time t + delta_t."""
         x_t = carry
         x_t = vmap_euler_step(x_t, time, delta_t)
         return x_t
     
     def body_fn_wrapper(time_idx, carry):
         """Convert time index to time and handle adaptive final integration step."""
-        cur_time = jnp.minimum(time_idx * delta_t, time)
-        prev_time = (time_idx - 1) * delta_t
-        actual_delta_t = cur_time - prev_time
-        return body_fn(cur_time, carry, actual_delta_t)
+        cur_time = (time_idx - 1) * delta_t
+        next_time = jnp.minimum(time_idx * delta_t, time)
+        adaptive_delta_t = next_time - cur_time
+        return body_fn(cur_time, carry, adaptive_delta_t)
         
     num_integration_steps = jnp.ceil(time / delta_t).astype(int)
     x_t = jax.lax.fori_loop(
@@ -110,26 +117,19 @@ def as_top_level_api(
     base_sample_fn: Callable,
     probability_path_logdensity_fn: Callable,
     num_samples: int = 1,
-    num_time_steps: int = 200,
-    delta_t: Scalar = 0.005,
 ):
     def init_fn(params: PyTree) -> LFISState:
         return init(params, optimizer)
     
-    def step_fn(
-        rng_key: Key, state: LFISState, time: Scalar
-    ) -> tuple[LFISState, LFISInfo]:
+    def step_fn(rng_key: Key, state: LFISState) -> tuple[LFISState, LFISInfo]:
         return step(
             rng_key=rng_key, 
-            state=state, 
-            time=time, 
+            state=state,
             optimizer=optimizer, 
             static=static, 
             base_sample_fn=base_sample_fn, 
             probability_path_logdensity_fn=probability_path_logdensity_fn, 
-            num_samples=num_samples, 
-            num_time_steps=num_time_steps,
-            delta_t=delta_t,
+            num_samples=num_samples,
         )
     
     def sample_fn(rng_key: Key, time: Scalar, params: PyTree) -> Array:
@@ -140,7 +140,6 @@ def as_top_level_api(
             static=static,
             base_sample_fn=base_sample_fn,
             num_samples=num_samples,
-            delta_t=delta_t,
         )
     
     return LFISAlgorithm(init_fn, step_fn, sample_fn)
--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-from typing import Callable, NamedTuple
-
 import jax
 import optax
 import wandb
-import seaborn as sns
 import equinox as eqx
 import jax.numpy as jnp
 import jax.random as jr
 import jax.scipy as jsp
 import matplotlib.pyplot as plt
-from jaxtyping import Array, Key, PyTree, Scalar
+import seaborn as sns
+from jaxtyping import Array, Key, Scalar
 
 from lfis import as_top_level_api
-from utils import (
-    continuity_error, compute_1D_wasserstein_distance, Velocity
-)
+from utils import compute_1D_wasserstein_distance, Velocity
 
 
 jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
@@ -45,16 +41,16 @@ def target_logdensity_fn(x: Array) -> Scalar:
 
 def target_sample_fn(rng_key, num_samples=1):
     m1_key, m2_key, m3_key, m4_key, cat_key = jr.split(rng_key, 5)
-    m1 = jr.multivariate_normal(m1_key, jnp.array([2.0, 2.0]), jnp.eye(2), shape=(num_samples,))
+    m1 = jr.multivariate_normal(m1_key, jnp.array([2.0, 2.0]), jnp.eye(2), shape=(num_samples,)) # shape (num_samples, dim)
     m2 = jr.multivariate_normal(m2_key, jnp.array([-2.0, -2.0]), jnp.eye(2), shape=(num_samples,))
     m3 = jr.multivariate_normal(m3_key, jnp.array([2.0, -2.0]), jnp.eye(2), shape=(num_samples,))
     m4 = jr.multivariate_normal(m4_key, jnp.array([-2.0, 2.0]), jnp.eye(2), shape=(num_samples,))
-    mixtures = jnp.stack([m1, m2, m3, m4], axis=1)
+    mixtures = jnp.stack([m1, m2, m3, m4], axis=1) # shape (num_samples, num_mixtures, dim)
     weights = jnp.array([0.25, 0.25, 0.25, 0.25])
     categories = jr.categorical(cat_key, weights, shape=(num_samples,))
-    one_hot_categories = jax.nn.one_hot(categories, num_classes=4)  # shape (num_samples, 4)
-    one_hot_expanded = jnp.expand_dims(one_hot_categories, axis=-1)  # shape (num_samples, 4, 1)
-    return jnp.sum(mixtures * one_hot_expanded, axis=1)  # shape (num_samples, 2)
+    one_hot_categories = jax.nn.one_hot(categories, num_classes=4)  # shape (num_samples, num_mixtures)
+    one_hot_expanded = jnp.expand_dims(one_hot_categories, axis=-1)  # shape (num_samples, num_mixtures, 1)
+    return jnp.sum(mixtures * one_hot_expanded, axis=1)  # shape (num_samples, dim)
 
 
 def probability_path_logdensity_fn(x: Array, time: Scalar) -> Scalar:
@@ -63,125 +59,58 @@ def probability_path_logdensity_fn(x: Array, time: Scalar) -> Scalar:
     return (1 - time) * base_logdensity_fn(x) + time * target_logdensity_fn(x)
 
 
-class LFISMetrics(NamedTuple):
-    true_wass: Array
-    approx_wass: Array
-    true_continuity_error: Array
-    approx_continuity_error: Array
-    
-
-def compute_metrics(rng_key, lfis, static, LFISState, LFISInfo) -> LFISMetrics:
+def compute_metrics(rng_key, lfis, state, info):
+    num_samples = info.x_t.shape[0]
     target_key, target_key2, approx_key = jr.split(rng_key, 3)
-    num_samples = LFISInfo.x_t.shape[0]
-    
     target_samples = target_sample_fn(target_key, num_samples)
     target_samples2 = target_sample_fn(target_key2, num_samples)
-    approx_samples = lfis.sample(approx_key, 1.0, LFISState.params)
-    
+    approx_samples = lfis.sample(approx_key, 1.0, state.params)
     true_wass = compute_1D_wasserstein_distance(target_samples, target_samples2)
     approx_wass = compute_1D_wasserstein_distance(target_samples, approx_samples)
-    
-    true_continuity_error = jnp.mean(
-        continuity_error(
-            LFISState.params, static, target_samples, 1.0, probability_path_logdensity_fn
-        ) ** 2
-    )
-    approx_continuity_error = jnp.mean(
-        continuity_error(
-            LFISState.params, static, approx_samples, 1.0, probability_path_logdensity_fn
-        ) ** 2
-    )
-    
+    plt.clf()
     figure = sns.scatterplot(x=approx_samples[:, 0], y=approx_samples[:, 1], alpha=0.5)
-    
     wandb.log(
         {
             "true_wass": true_wass,
             "approx_wass": approx_wass,
-            "true_continuity_error": true_continuity_error,
-            "approx_continuity_error": approx_continuity_error,
             "approx_samples": wandb.Image(figure.get_figure()),
+            "time": info.time,
+            "loss": info.loss,
         }
     )
-    return LFISMetrics(
-        true_wass=true_wass,
-        approx_wass=approx_wass,
-        true_continuity_error=jnp.mean(true_continuity_error ** 2),
-        approx_continuity_error=jnp.mean(approx_continuity_error ** 2),
-    )
 
 
-def main(
-    seed: int, 
-    num_time_steps: int, 
-    num_train_steps: int, 
-    num_samples: int, 
-    delta_t: Scalar
-):
+def main(seed: int, num_train_steps: int, num_samples: int):
     rng_key = jr.key(seed)
     rng_key, nn_key = jr.split(rng_key, 2)
-    
     velocity = Velocity(in_size=dim + 1, out_size=dim, key=nn_key)
     params, static = eqx.partition(velocity, eqx.is_inexact_array)
     optimizer = optax.adam(1e-3)
-    
     lfis = as_top_level_api(
         optimizer=optimizer,
         static=static,
         base_sample_fn=base_sample_fn,
         probability_path_logdensity_fn=probability_path_logdensity_fn,
         num_samples=num_samples,
-        num_time_steps=num_time_steps,
-        delta_t=delta_t,
     )
     state = lfis.init(params)
 
-    for i in range(num_time_steps):
-        time_key = jr.fold_in(rng_key, i)
-        time = jr.uniform(time_key) # shape ()
-        # time = jnp.array((i + 1) / num_time_steps) # shape ()
-        # time = jnp.linspace(0.4, 0.6, num_time_steps)
-        print(f"\nIter {i} at time: {time}")
-
-        for j in range(num_train_steps):
-            step_key = jr.fold_in(time_key, j)
-            state, info = lfis.step(
-                rng_key=step_key,
-                state=state,
-                time=time,
-            )
-            if j % 50 == 0:
-                print(f"Step {j} Loss: {info.loss}")
-        
-        metrics_key = jr.fold_in(time_key, 1)
-        metrics = compute_metrics(metrics_key, lfis, static, state, info)
-        print(f"\tWass Dist: {metrics.approx_wass:.4f} ({metrics.true_wass:.4f})")
-        print(f"\tContinuity Error: {metrics.approx_continuity_error:.4f} ({metrics.true_continuity_error:.4f})")
-
-    # generate approximate samples and plot them
-    rng_key, sub_key = jr.split(rng_key)
-    velocity = eqx.combine(state.params, static)
-    approx_samples = lfis.sample(sub_key, 1, state.params)
-    sns.scatterplot(x=approx_samples[:, 0], y=approx_samples[:, 1], alpha=0.5)
-    plt.savefig('hist.png', dpi=300)
+    for i in range(num_train_steps):
+        time_key, metrics_key = jr.split(jr.fold_in(rng_key, i))
+        state, info = lfis.step(time_key, state)
+        compute_metrics(metrics_key, lfis, state, info)
 
 
 if __name__ == "__main__":
     seed = 12345
-    num_time_steps = 200
     num_train_steps = 500
-    num_samples = 1000
-    delta_t = 0.005
-    
+    num_samples = 10000
     wandb.init(
         project="lfis",
         config={
             "seed": seed,
-            "num_time_steps": num_time_steps,
             "num_train_steps": num_train_steps,
             "num_samples": num_samples,
-            "delta_t": delta_t,
         },
     )
-    
-    main(seed, num_time_steps, num_train_steps, num_samples, delta_t)
+    main(seed, num_train_steps, num_samples)
